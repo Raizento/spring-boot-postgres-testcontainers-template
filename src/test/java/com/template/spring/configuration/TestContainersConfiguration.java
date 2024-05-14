398576f6bfde2a1ad4692ddb1c23094da63f237e
Raizento <it.alexander.molitor@gmail.com>
2024-05-14 21:11:41 +0200

    Added devtools

--- src/test/java/com/template/spring/configuration/TestContainersConfiguration.java
+++ src/test/java/com/template/spring/configuration/TestContainersConfiguration.java
@@ -1,5 +1,6 @@
 package com.template.spring.configuration;
 
+import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ public class TestContainersConfiguration {
 
     @Bean
     @ServiceConnection
+    @RestartScope
     public PostgreSQLContainer<?> postgreSQLContainer() {
         // You can create a startup script for populating the DB by creating a
         // `init.sql` file in the test/resources dir
